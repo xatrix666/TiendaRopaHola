938d876a56dd133134ebec5ce547c5a724775873
Xavier Valles <xvv1988@gmail.com>
2024-05-24 11:58:54 +0200

    Add project files.

---
+++ TiendaRopaHola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaRopaHola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
